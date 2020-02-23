54d0d90e0880d4ca3c377c804797bc64e5bc19d4
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-22 19:06:32 -0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Recipe } from './model/recipe';
+
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,5 @@ import { Recipe } from './model/recipe';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    recipes: Recipe[];
 
-    constructor() {
-      this.recipes = [
-        new Recipe ('Banana bread', 'Lorem ipsum banana bread', null, null, null),
-        new Recipe ('French Toast', 'This is banana bread', null, null, null),
-        new Recipe ('BBQ Chicken', 'This is banana bread', null, null, null),
-        new Recipe ('Grilled Pineapple', 'This is banana bread', null, null, null)
-      ];
-    }
 }
